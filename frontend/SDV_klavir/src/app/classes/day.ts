5cba93e9372c6be50f343a5afd75d1a1a129c00e
ba-na-na-na-na <vojko.hysz@gmail.com>
2025-09-19 07:45:28 +0200

    class changes

--- frontend/SDV_klavir/src/app/classes/day.ts
+++ frontend/SDV_klavir/src/app/classes/day.ts
@@ -1,13 +1,23 @@
 import { Timeslot } from "./timeslot";
 
 export interface Day {
-    id: string;
+    _id: string;
     date: Date;	
     dayOfTheWeek: number;
     timeslots: Timeslot[];
 }
 
-export interface DayPlaceholder {
+interface DayPlaceholder {
     name: string;
     dayOfTheWeek: number;
 }
+
+export const days2Names : DayPlaceholder[] = [
+    { name: "Ponedeljek", dayOfTheWeek: 1 },
+    { name: "Torek", dayOfTheWeek: 2 },
+    { name: "Sreda", dayOfTheWeek: 3 },
+    { name: "Četrtek", dayOfTheWeek: 4 },
+    { name: "Petek", dayOfTheWeek: 5 },
+    { name: "Sobota", dayOfTheWeek: 6 },
+    { name: "Nedelja", dayOfTheWeek: 0 }
+];
--- frontend/SDV_klavir/src/app/classes/timeslot.ts
+++ frontend/SDV_klavir/src/app/classes/timeslot.ts
@@ -1,6 +1,7 @@
 import { ReservationType } from './reservation-type';
 
 export interface Timeslot {
+    _id: string;
     timeslotNo: number;
     start: string;
     reservationType: ReservationType;
