6e71252f46af16fc3117330fa2f4a3b206d8f7df
ba-na-na-na-na <vojko.hysz@gmail.com>
2025-09-22 10:11:14 +0200

    modal tweaks

--- frontend/SDV_klavir/src/app/components/dayslot/dayslot.component.ts
+++ frontend/SDV_klavir/src/app/components/dayslot/dayslot.component.ts
@@ -42,6 +42,12 @@ export class DayslotComponent implements OnInit {
     this.getAllTimeslots();
   }
 
+  protected onBackgroundClick(event: MouseEvent) {
+    if (event.target === event.currentTarget) {
+      this.hideModal();
+    }
+  }
+
   protected getAllTimeslots(): void {
     this.dayService.getDay(this.dayNumber).subscribe(day => {
       this.day = day;
@@ -131,116 +137,10 @@ export class DayslotComponent implements OnInit {
   }
 
   protected showModal() {
-    this.modal.nativeElement.style.display = 'block'
+    this.modal.nativeElement.style.display = 'block';
   }
 
   protected hideModal() {
-    this.modal.nativeElement.style.display = 'none'
+    this.modal.nativeElement.style.display = 'none';
   }
-
-  //  {
-  //   _id: "68cb9ba577307b421d3ce495",
-  //   date: new Date("2025-09-19T05:45:55.666Z"),
-  //   dayOfTheWeek: 5,
-  //   timeslots: [
-  //     {
-  //       timeslotNo: 0,
-  //       start: "1",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce496",
-  //       reservedBy: "nobody"
-  //     },
-  //     {
-  //       timeslotNo: 1,
-  //       start: "2",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce497",
-  //       reservedBy: "nobody"
-  //     },
-  //     {
-  //       timeslotNo: 2,
-  //       start: "3",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce498",
-  //       reservedBy: "nobody"
-  //     },
-  //     {
-  //       timeslotNo: 3,
-  //       start: "4",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce499",
-  //       reservedBy: "nobody"
-  //     },
-  //     {
-  //       timeslotNo: 4,
-  //       start: "5",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce49a",
-  //       reservedBy: "nobody"
-  //     },
-  //     {
-  //       timeslotNo: 5,
-  //       start: "6",
-  //       reservationType: ReservationType.Temporary,
-  //       _id: "68cb9ba577307b421d3ce49b",
-  //       reservedBy: "Katja"
-  //     },
-  //     {
-  //       timeslotNo: 6,
-  //       start: "7",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce49c",
-  //       reservedBy: "nobody"
-  //     },
-  //     {
-  //       timeslotNo: 7,
-  //       start: "8",
-  //       reservationType: ReservationType.Unreserved,
-  //       reservedBy: "nobody",
-  //       _id: "68cb9ba577307b421d3ce49d",
-  //     },
-  //     {
-  //       timeslotNo: 8,
-  //       start: "9",
-  //       reservationType: ReservationType.Permanent,
-  //       reservedBy: "Vojko",
-  //       _id: "68cb9ba577307b421d3ce49e"
-  //     },
-  //     {
-  //       timeslotNo: 9,
-  //       start: "10",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce49f",
-  //       reservedBy: "nobody"
-  //     },
-  //     {
-  //       timeslotNo: 10,
-  //       start: "11",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce4a0",
-  //       reservedBy: "nobody"
-  //     },
-  //     {
-  //       timeslotNo: 11,
-  //       start: "12",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce4a1",
-  //       reservedBy: "nobody"
-  //     },
-  //     {
-  //       timeslotNo: 12,
-  //       start: "13",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce4a2",
-  //       reservedBy: "nobody"
-  //     },
-  //     {
-  //       timeslotNo: 13,
-  //       start: "14",
-  //       reservationType: ReservationType.Unreserved,
-  //       _id: "68cb9ba577307b421d3ce4a3",
-  //       reservedBy: "nobody"
-  //     }
-  //   ]
-  // }
 }
