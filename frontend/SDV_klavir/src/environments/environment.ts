148f3ab7fa4a8dc85ec46f97632b28bad4868b1e
ba-na-na-na-na <vojko.hysz@gmail.com>
2025-10-06 11:52:25 +0000

    deployment little fixes

--- frontend/SDV_klavir/src/environments/environment.ts
+++ frontend/SDV_klavir/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  apiUrl: 'http://zmajkotrek.si/v1',
-  socketUrl: 'http://zmajkotrek.si',
+  apiUrl: 'https://zmajkotrek.si/v1',
+  socketUrl: 'https://zmajkotrek.si',
   // TO-DO change for production ->> https://zmajkotrek.si
   production: true,
 };
